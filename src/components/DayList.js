42d1aaa7f04d349eb5d7e957bab274ac577677a6
Emily Lim <emily.elim04@gmail.com>
2021-02-15 20:24:08 -0500

    build daylist

---
+++ src/components/DayList.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props){
+
+  const {days} = props;
+
+  const parsedDays = days.map((day) => {
+    return (
+      <DayListItem 
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    )
+  })
+
+  return (
+    <ul>
+      {parsedDays}
+    </ul>
+  )
+}
