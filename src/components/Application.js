65900b3fbc6dd6570adf06e453ccd59d7f0338c6
Emily Lim <emily.elim04@gmail.com>
2021-02-16 20:06:42 -0500

    add to schedule in application

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,43 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+  },
+  {
+    id: 4,
+    time: "10am",
+    interview: {
+      student: "Grogu",
+      interviewer: {
+        id: 2,
+        name: "Mando",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
+];
+
 
 const days = [
   {
@@ -25,6 +62,8 @@ const days = [
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+
+  const parsedApps = appointments.map( (appointment) => <Appointment key={appointment.id} {...appointment} />);
 
   return (
     <main className="layout">
@@ -49,6 +88,8 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
+      {parsedApps}
+      <Appointment key="last" time="5pm" />
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React from "react";
 
 import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
@@ -8,7 +8,7 @@ import Empty from "components/Appointment/Empty";
 export default function Appointment(props) {
 
   const toDisplay = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>;
-
+  console.log(props)
   return (
     <article className="appointment">
       <Header time={props.time}/>
