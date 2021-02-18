22db0153dd30e2003956362c44072d116e78cf0a
Emily Lim <emily.elim04@gmail.com>
2021-02-18 17:53:06 -0500

    create custom hooks for transitions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+export default function useVisualMode(initial) {
+
+const [mode, setMode] = useState(initial);
+const [history, setHistory] = useState([initial]);
+
+const transition = (newMode, replace = false) => {
+
+  if (replace) {
+    const newHistory = [...history];
+    setMode(newHistory[newHistory.length - 1])
+    setHistory(newHistory.slice(0, -1))
+  }
+  
+    setMode(newMode);
+    setHistory(history => [...history, newMode]);
+}
+
+const back = () => {
+
+  setMode(history.length > 1 ? history[history.length - 2] : history[history.length - 1])
+
+  setHistory(history => history.length > 1 ? history.slice(0, -1) : history)
+
+}
+
+  return {mode, transition, back};
+}
+
