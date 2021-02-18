046996f21616bafafd871f89a45ab950b2a9aac6
Emily Lim <emily.elim04@gmail.com>
2021-02-18 17:52:40 -0500

    revise appointment index to use useVisuualMode function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,15 +4,41 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "../../hooks/useVisualMode.js";
+import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
 
-  const toDisplay = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>;
-  console.log(props)
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";  
+
+  const {mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  // const toDisplay = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>;
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {toDisplay}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show 
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && 
+        <Form 
+        name={props.name}
+        interviewers={[]}
+        value={props.value}
+        onSave={() => console.log("onSave")}
+        onCancel={() => back()}
+        />
+      }
+    
     </article>
   )
 
