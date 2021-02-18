63af5cadfe80316fdf27fcdd384f6c4f5bb67902
Emily Lim <emily.elim04@gmail.com>
2021-02-18 18:59:47 -0500

    add other modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,12 +6,18 @@ import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import useVisualMode from "../../hooks/useVisualMode.js";
 import Form from "components/Appointment/Form";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 export default function Appointment(props) {
 
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
-  const CREATE = "CREATE";  
+  const CREATE = "CREATE"; 
+  const CONFIRM = "CONFIRM";
+  const ERROR = "ERROR";
+  const STATUS = "STATUS"; 
 
   const {mode, transition, back} = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -32,10 +38,27 @@ export default function Appointment(props) {
       {mode === CREATE && 
         <Form 
         name={props.name}
-        interviewers={[]}
+        interviewers={props.interviewers}
         value={props.value}
-        onSave={() => console.log("onSave")}
-        onCancel={() => back()}
+        // onSave={onSave}
+        onCancel={back}
+        />
+      }
+      {mode === CONFIRM && 
+        <Confirm
+          message="Are you sure you would like to delete?"
+          // onConfirm={onDelete}
+        />
+      }
+      {mode === STATUS && 
+        <Status
+          message="Saving..."
+        />
+      }
+      {mode === ERROR &&
+        <Error
+          message="Could not delete appointment."
+          onClose={back}
         />
       }
     
