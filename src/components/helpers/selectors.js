c118f2898504e90f7f36e22d175ccbd357cad8f6
Emily Lim <emily.elim04@gmail.com>
2021-02-18 17:51:23 -0500

    implement get interview helper function and test

--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -3,8 +3,22 @@ export function getAppointmentsForDay(state, day) {
 
   const filteredDay = state.days.find((currentDay) => currentDay.name === day);
 
+  //if filteredDay exists, then use map to replace array of ids with the appointment ids and return
   const filteredApp = filteredDay ? filteredDay.appointments.map((id) => state.appointments[id]) : [];
 
   return filteredApp;
 
+}
+
+export function getInterview({ interviewers }, interview) {
+
+  if (!interview) {
+    return null;
+  }
+
+  const interviewInfo = interviewers[interview.interviewer];
+
+  const finalInterview = {...interview, interviewer: interviewInfo};
+
+  return finalInterview;
 }
