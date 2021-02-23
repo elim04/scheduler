38e900bb44e188178e2825878da917a8ecd676a0
Emily Lim <emily.elim04@gmail.com>
2021-02-22 21:05:45 -0500

    add new test and mock axios put request

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -81,7 +81,13 @@ export default {
         data: fixtures.interviewers
       });
     }
-
-
+  }),
+  put: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    })
+    
   })
+
 }
